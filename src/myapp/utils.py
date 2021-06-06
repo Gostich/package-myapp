baebb54e323ade95608342b9b65e394fa528d912
Gosti <yannick.gosteli@petzi.ch>
2021-06-06 16:13:02 +0200

    create utils.format_hello

---
+++ src/myapp/utils.py
@@ -0,0 +1,2 @@
+def format_hello(name):
+    return f"Hello {name}"
--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 
 from .models import Place
+from .utils import format_hello
 
 
 def hello_world(request):
-    hello_list = [f"Hello {place}" for place in Place.objects.all()]
+    hello_list = [format_hello(place.name) for place in Place.objects.all()]
     if not hello_list:
         return HttpResponse("No place :-(")
     return HttpResponse(" - ".join(hello_list))
