e25516bf724312c415e93904598d3fa34823d38a
Gosti <yannick.gosteli@petzi.ch>
2021-06-06 15:20:06 +0200

    say hello to everybody

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import Place
+
 
 def hello_world(request):
-    return HttpResponse("Hello world")
+    hello_list = [f"Hello {place}" for place in Place.objects.all()]
+    if not hello_list:
+        return HttpResponse("No place :-(")
+    return HttpResponse(" - ".join(hello_list))
